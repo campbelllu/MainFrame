5b62ed2c295e69f0b0dec349be74aae0052d5fed
Luke <campbelllu3@gmail.com>
2023-06-27 20:36:59 -0700

    Holy moly! Ingress works, I can use minikube tunnel now instead of port forwarding, but I need to test host files, and routing URL's/addresses to figure out what all is being handled by k8's ingress and what's being handled by Django URL definitions, how much redundancy there is, if any. But deployments work, docker images work, services work, ingress is working, I'm just a little fuzzy on how the networking is currently sustained and managed. So exciting! Plan: Ingress de-fog-of-war'ing, then Jenkins setup. Then learning how Jenkins updates k8's-hosted MF images as they're updated. Then it's onto Ansible/Pulumigit status! Great start to the week!

--- mainframe/mainframe/settings.py
+++ mainframe/mainframe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0owy4u$f=_w+zl8h%$p320c(v$4$skq2_%xpdre&m-gmddjxdl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','mf.io']
 
 
 # Application definition
