d91c09e37d4425d2716c5c08b7a9d073dc18e13e
Luke <campbelllu3@gmail.com>
2024-02-13 22:12:02 -0700

    Preliminary file splitting, testing API's, planning calculations. Needs cleanup, pseudo translated, and testing.

---
+++ mainframe/csv_modules.py
@@ -0,0 +1,120 @@
+import numpy as np
+import pandas as pd
+import pandas_datareader.data as web
+#docu: https://pandas-datareader.readthedocs.io/en/latest/ 
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+# %matplotlib inline
+import datetime as dt
+import mplfinance as mpf
+import datetime as dt
+import time
+import yfinance as yf
+import json
+
+#save any data frame into a csv for later use. Param's: save location, df to be saved, name of csv as string
+def simple_saveDF_to_csv(folder, df, name:STRING):
+    try:
+        df.to_csv(folder + name + '.csv')
+    except Exception as err:
+        print("Simple Save to CSV Error Message:")
+        print(err)
+
+#load csv's into dataframe
+def simple_get_df_from_csv(folder, name:STRING):
+    try:
+        df = pd.read_csv(folder + name + '.csv')
+    except FileNotFoundError as err:
+        print("File Does Not Exist")
+    else:
+        return df
+
+#Returns Named Column Data from CSV
+def get_column_from_csv(file, col_name):
+    #Get file, else throw a warning
+    try:
+        df = pd.read_csv(file)
+    except FileNotFoundError:
+        print("File Does Not Exist")
+        # print("Error message: \n" + err)
+    else:
+        return df[col_name]
+
+# Function returns a dataframe by providing a ticker and starting date, and saves it to csv
+def save_to_csv_from_yahoo(folder, ticker, syear, smonth, sday, eyear, emonth, eday):
+    # Defines the time periods to use
+    # start = int(dt.datetime(syear, smonth, sday).strftime("%Y%m%d"))
+    # end = int(dt.datetime(eyear, emonth, eday).strftime("%Y%m%d%H%M%S"))
+    start = dt.datetime(syear, smonth, sday)
+    end = dt. datetime(eyear, emonth, eday)
+    try:
+        print("Getting Data for: ", ticker)
+
+        # #DEBUG
+        # print("debugging s2csvfyahoo: st, end, df : ")
+        # print(start)
+        # print(end)
+        # Reads data into a dataframe
+        # df = web.DataReader(ticker, 'yahoo', start, end)#['Adj Close']
+        df = yf.download(ticker, start, end)
+        # #DEBUG
+        # print("debugging s2csvfyahoo: st, end, df : ")
+        # print(start)
+        # print(end)
+        # print(df)
+
+        #Wait to avoid kick
+        time.sleep(3)
+        # Save data to a CSV file
+        # For Windows
+        # df.to_csv('C:/Users/derek/Documents/Python Finance/Python/' + ticker + '.csv')
+        # For MacOS/Unix
+        df.to_csv(folder + ticker + '.csv')
+    except Exception as err:
+        stocks_not_downloaded.append(ticker)
+        print("Couldn't Get Data for :", ticker)
+        print("Error message:")
+        print(err)
+    # return df
+
+# Reads a dataframe from the CSV file, changes index to date and returns it
+def get_df_from_csv(folder, ticker):
+    # Try to get the file and if it doesn't exist issue a warning
+    try:
+        # For Windows
+        # df = pd.read_csv('C:/Users/derek/Documents/Python Finance/Python/' + ticker + '.csv')
+        # For MacOS
+        df = pd.read_csv(folder + ticker + '.csv')
+    except FileNotFoundError as err:
+        print("File Does Not Exist")
+        # print("Error message: \n" + err)
+    else:
+        return df
+
+#hold stocks not downloaded on original pass through-download
+stocks_not_downloaded = []
+missing_stocks = []
+
+### USING THE ABOVE
+# tickers = get_column_from_csv("./full_commonstock_names.csv", "Symbol")
+# print(tickers)
+
+# folder = "./stock_data/"
+# 
+# for x in range(1):
+#     save_to_csv_from_yahoo(folder, tickers[x],2020,1,1,2024,1,1)
+# print("stocks not dl'd:")
+# print(stocks_not_downloaded)
+# print("missing stocks:")
+# print(missing_stocks)
+
+# msft = yf.Ticker("MSFT", period="10y")
+# incStatTest = pd.DataFrame(msft.income_stmt)
+
+# # print(incStatTest)
+# incStatTest.to_csv("./stock_data/msftIncTest.csv")
+
+
+# 
+# 
+#####
--- mainframe/v1_ic.py
+++ mainframe/v1_ic.py
@@ -1,30 +1,58 @@
 import numpy as np
 import pandas as pd
-from pandas_datareader import data as web
+import pandas_datareader.data as web
+#docu: https://pandas-datareader.readthedocs.io/en/latest/ 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 # %matplotlib inline
 import datetime as dt
 import mplfinance as mpf
+import datetime as dt
+import time
+import yfinance as yf
+import json
+import pyarrow
 
+# Full downloaded list of every equity in USA
 df_SymbolsAndNames = pd.read_csv('./full_stock_list.csv', usecols=['Symbol','Name'])
-df_FilteredList = pd.DataFrame()
+# df_FilteredList = pd.DataFrame() #unused, feels cute, might delete later
 # print(df)
-# Now we need to iterate through the CSV, Name column, IF containing 'Common Stock', we keep it and add it to new df
-def filter_stocks():
-    df2 = df_SymbolsAndNames[df_SymbolsAndNames['Name'].str.contains('Common Stock')]#pd.DataFrame()
-    # for row in df_SymbolsAndNames:
-    # if df_SymbolsAndNames["B"].str.contains('Common Stock'):
-    #     df2.assign()
-    # if df_SymbolsAndNames[df_SymbolsAndNames["B"].str.contains('Common Stock')]:
-    #     df2.assign(row)
-        # if row.contains('Common Stock'):
-        #     df2.assign(row)
-    
-    return df2
 
-df_FilteredList = filter_stocks()
-df_FilteredList.to_csv('full_commonstock_names.csv', index=False)
-print('csv created!')
-# print(df_FilteredList)
-# print(df_FilteredList)
+# Iterate through CSV, Name column, IF containing 'Common Stock', we keep it and add it to new, returned, df
+def filter_onlyCommonStocks(df_full):
+    df2 = df_full[df_full['Name'].str.contains('Common Stock')]#pd.DataFrame()    
+    return df2
+# df_FilteredList = filter_onlyCommonStocks(df_SymbolsAndNames)
+# df_FilteredList.to_csv('full_commonstock_names.csv', index=False)
+# print('csv created!')
+
+#We have to parse full SEC CIK list and append CIK's to full_commonstock_names.csv
+#add column to fcsn for CIK
+# df_fcsn = pd.read_csv("./full_commonstock_names.csv")
+# df_fcsn['CIK'] = "" 
+# df_fcsn.to_csv('./full_commonstock_names2.csv', index=False)
+
+#make fcl.json into a python dictionary
+json_path = './full_cik_list.json'
+with open(json_path, 'r') as j:
+    fcl_dict = json.loads(j.read())
+# print(fcl_dict["0"]['cik_str'])
+def addCIKtoStockNameList():
+
+    return 0
+#for y in fcl, get "ticker" value=y
+#for x in fcsn, if df['Ticker'] == y
+#
+
+#From Video 1
+
+dgro = yf.Ticker("MSFT")
+dict = dgro.info
+df = pd.DataFrame.from_dict(dict,orient='index')
+df = df.reset_index()
+df.to_csv('./stock_data/msftTest.csv', index=False)
+# print(df)
+
+        
+
+
