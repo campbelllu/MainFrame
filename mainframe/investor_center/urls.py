78c93f6e47bc882249ec72176adafd7f57be2b19
campbelllu <campbelllu3@gmail.com>
2024-08-06 22:14:45 -0700

    learning django templating!

--- mainframe/investor_center/urls.py
+++ mainframe/investor_center/urls.py
@@ -22,6 +22,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('v1/', views.v1, name='v1'),
+    path('md/', views.metadata, name='md'),
    
 ]
--- mainframe/investor_center/views.py
+++ mainframe/investor_center/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from django.template import loader
+
+from .models import Metadata, Sector_Rankings
 
 # Create your views here.
 def index(request):
     return render(request, 'investor_center/index.html', {})
     #HttpResponse('<h1>Welcome to The Investor Center.</h1>')
 
-def v1(request):
-    return HttpResponse('<h1>One Piston. Very unique ICEngine.</h1>')
+def metadata(request):
+    wholeTable = Sector_Rankings.objects.order_by('Sector', '-scorerank')[:25]
+    
+    context = {
+        'wt': wholeTable,
+    }
+    return render(request, 'investor_center/metadata.html', context)
+    #template = loader.get_template("investor_center/metadata.html")
+    #HttpResponse(template.render(context,request))
+        #'<h1>One Piston. Very unique ICEngine.</h1>')
