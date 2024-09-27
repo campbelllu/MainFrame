cab9e1a48b69704d711dab5a29034c457b6af41a
Luke <campbelllu3@gmail.com>
2024-09-27 15:23:27 -0700

    starting the css journey.

--- mainframe/mainframe/settings.py
+++ mainframe/mainframe/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'investor_center/static/' #os.path.join(BASE_DIR, 'investor_center/templates/investor_center/')
+
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
