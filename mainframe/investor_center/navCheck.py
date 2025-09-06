e25821ba31ef11da628031895aa3d393de9e4c7e
Luke <campbelllu3@gmail.com>
2025-09-05 22:51:47 -0700

    TR calcs are off. fix that and i think we got it.

--- mainframe/investor_center/navCheck.py
+++ mainframe/investor_center/navCheck.py
@@ -28,13 +28,17 @@ import yfinance as yf
 ###################################################
 
 # Define ETFs to compare 
-etfs = ['VONG', "QDVO"] # # Example ETFs ; iwy==ymag ##First underlying, second the one to test against it
-time_period = "1y" #mo, y
+etfs = ['SPY', "SPYI"] # # Example ETFs ; iwy==ymag ##First underlying, second the one to test against it
+time_period = "3y" #mo, y
+start_date = "2023-01-01"
+end_date = "2024-12-31"
 
 # Fetch historical price data for each ETF 
 etf_data = {} 
 for etf in etfs: 
-    data = yf.Ticker(etf).history(period=time_period) # Fetch 1 year of data 
+    # data = yf.Ticker(etf).history(period=time_period) # Fetch data from time_period
+    data = yf.Ticker(etf).history(start=start_date, end=end_date) # Fetch data from start to end date
+
   
     etf_data[etf + '-Close'] = data["Close"] # Store only closing prices 
     etf_data[etf + '-Divs'] = data['Dividends']
@@ -42,39 +46,35 @@ for etf in etfs:
     etf_data[etf + '-TRScuffed'] = round(data['Close'] + data['Dividends'].cumsum(), 5)
     TRAlist = []
     TRA_pct_change_list = []
+    price_return = []
+    price_return_pct_change = []
+    first_close_price = etf_data[etf + '-Close'].iloc[0]
 
     for i in range(len(etf_data[etf + '-Close'])):        
         #Finding Total Return at that time, similar to the equation below this: 
-        TRA = etf_data[etf + '-TRScuffed'].iloc[i] - etf_data[etf + '-Close'].iloc[0]
+        TRA = etf_data[etf + '-TRScuffed'].iloc[i] - first_close_price
         # ((end price - initial price) + distributions over that time) / initial price
         #((data["Close"].iloc[i] - data["Close"].iloc[0]) + data['Dividends'].iloc[0].cumsum()) / data["Close"].iloc[0]
-
         TRAlist.append(round(TRA,5))
-
-        TRA_pct_change = TRA / etf_data[etf + '-Close'].iloc[0] * 100
-
+        TRA_pct_change = TRA / first_close_price * 100
         TRA_pct_change_list.append(round(TRA_pct_change,5))
 
-    etf_data[etf + '-TRActual'] = TRAlist
-    etf_data[etf + '-TR_pct_change'] = TRA_pct_change_list
-
-    #add price change and percentage change columns
-    price_return = []
-    price_return_pct_change = []
-    for i in range(len(etf_data[etf + '-Close'])):
-        priceReturn = etf_data[etf + '-Close'].iloc[i] - etf_data[etf + '-Close'].iloc[0]
+        #Finding price change data
+        priceReturn = etf_data[etf + '-Close'].iloc[i] - first_close_price
         price_return.append(round(priceReturn,5))
-        PR_pct_change = priceReturn / etf_data[etf + '-Close'].iloc[0] * 100
+        PR_pct_change = priceReturn / first_close_price * 100
         price_return_pct_change.append(round(PR_pct_change,5))
 
     etf_data[etf + '-PriceReturn'] = price_return
     etf_data[etf + '-PR_pct_change'] = price_return_pct_change
-    # etf_data[etf + '-AdjClose'] = data['Close'] * (1 - (data['Dividends'].cumsum() / data['Close'].iloc[0])) # adjusted closing prices approximation #LUKE CUMSUM DROP IT I THINK # data["Close"] - data["Dividends"]# 
-    # etf_data[etf + '-PricePlusDivThatDate'] = data['Close'] + data["Dividends"]#.cumsum() #everything drip'd
+    etf_data[etf + '-TRActual'] = TRAlist
+    etf_data[etf + '-TR_pct_change'] = TRA_pct_change_list
 
-#Fixing TR divide by zero error
-etf_data[etfs[0]+ '-TRActual'][0] = 1.00000
-etf_data[etfs[1] + '-TRActual'][0] = 0.00001
+#Fixing TR and PR divide by zero error
+etf_data[etfs[0]+ '-TR_pct_change'][0] = 1.00000
+etf_data[etfs[1] + '-TR_pct_change'][0] = 0.00001
+etf_data[etfs[0]+ '-PR_pct_change'][0] = 1.00000
+etf_data[etfs[1] + '-PR_pct_change'][0] = 0.00001
 # df_etfD = pd.DataFrame(etf_data)
 # print(df_etfD.to_string())
 
@@ -85,49 +85,42 @@ df = df.reset_index()
 # Rename columns for clarity 
 df.columns = ["Date"] + list(etf_data.keys())
 
+#correlation between prices, then total returns
+df['Price_Correlation'] = df[etfs[1] + '-PR_pct_change'].corr(df[etfs[0] + '-PR_pct_change'])
+df['TR_Correlation'] = df[etfs[1] + '-TR_pct_change'].corr(df[etfs[0] + '-TR_pct_change'])
 #Create comparisons, normalize
 df['CloseComparison'] = df[etfs[1] + '-Close'] / df[etfs[0] + '-Close']
 df['CloseNorm_compare'] = df['CloseComparison'] / df['CloseComparison'].iloc[0]
-# df['AdjComparison'] = df[etfs[1] + '-AdjClose'] / df[etfs[0] + '-AdjClose']
-# df['AdjNorm_compare'] = df['AdjComparison'] / df['AdjComparison'].iloc[0]
-df['TRComparison'] = df[etfs[1] + '-TRActual'] - df[etfs[0] + '-TRActual']
-
-# df['TRComparison'] = df['TRComparison'].fillna(0)
-# print(df['TRComparison'].to_string())
-
-# guh = []
-# for i in range(len(df['TRComparison'])):   
-#     print(df['TRComparison'].iloc[i] / df['TRComparison'].iloc[0])
+#alt price comparison, redundant
+# df['CloseComparison_pct'] = df[etfs[1] + '-PR_pct_change'] / df[etfs[0] + '-PR_pct_change']
+# df['CloseNorm_compare_pct'] = df['CloseComparison_pct'] / df['CloseComparison_pct'].iloc[1]
+#TR compare
+df['TRComparison'] = df[etfs[1] + '-TR_pct_change'] / df[etfs[0] + '-TR_pct_change']
 df['TRNorm_compare'] = (df['TRComparison'] / df['TRComparison'].iloc[1])
+
+# print(df[['TRComparison', 'TRNorm_compare']].to_string())
 # print(df['TRNorm_compare'].to_string())
 
 # print(df[['Date', etfs[1]+'-TRActual',etfs[0]+'-TRActual','TRComparison','TRNorm_compare',]].to_string()) #etfs[1]+'-DivsSummed',etfs[0]+'-DivsSummed'
 # print(df[['Date', etfs[1]+'-PriceReturn', etfs[1]+'-PR_pct_change' ,etfs[0]+'-PriceReturn', etfs[0]+'-PR_pct_change']].to_string())
 # print(df[['Date', etfs[1]+'-TRActual', etfs[1]+'-TR_pct_change', etfs[0]+'-TRActual', etfs[0]+'-TR_pct_change']].to_string())
 # print(df[['Date', etfs[1]+'-PriceReturn', etfs[1]+'-PR_pct_change', etfs[1]+'-TRActual', etfs[1]+'-TR_pct_change']].to_string())
+# print(df[['Date', etfs[1] + '-PR_pct_change', etfs[0] + '-PR_pct_change', 'CloseComparison_pct', 'CloseNorm_compare_pct']].to_string())
 
 #LUKE
-#run this to see what we were seeing mid 2025
-#huge volatility, qqq goes negative, it screws up the ratio and forces it negative, even though it should be looking at absolute value changes, and that should boost rating. 
-#find a way to express their relationship via absolute values and you got it. TR is different from price returns because of those negative values. you can do it!
+#TR is wrong somehow. total return isn't calculated correctly because the tested's are beating the underlying by way too much. inaccurate twice.
 
 #Get percentage of time that fund beat the underlying
-# countOverOne = (df["AdjNorm_compare"] > 1).sum() 
-# percentOverOne = (df["AdjNorm_compare"] > 1).mean() * 100
-# percentOverOneLastTen = (df["AdjNorm_compare"].iloc[-10:] > 1).mean() * 100
 percentOverOne = (df["CloseNorm_compare"] > 1).mean() * 100
+# percentOverOne_pct = (df["CloseNorm_compare_pct"] > 1).mean() * 100
 # percentOverOneLastTen = (df["CloseNorm_compare"].iloc[-10:] > 1).mean() * 100
 TRpercentOverOne = (df["TRNorm_compare"] > 1).mean() * 100
 # TRpercentOverOneLastTen = (df["TRNorm_compare"].iloc[-10:] > 1).mean() * 100
 
-#Was I going for total return comparisons? delete because redundant now?
-# etf1gain = (df[etfs[1] + '-AdjClose'].iloc[-1] - df[etfs[1] + '-AdjClose'].iloc[0]) / df[etfs[1] + '-AdjClose'].iloc[0] * 100
-# etf0gain = (df[etfs[0] + '-AdjClose'].iloc[-1] - df[etfs[0] + '-AdjClose'].iloc[0]) / df[etfs[0] + '-AdjClose'].iloc[0] * 100
-# percentDifferent = (etf0gain - etf1gain) / etf1gain * 100
-
 #growth rates
 # df['PriceGrowthRate'] = df["AdjNorm_compare"].pct_change() * 100
 df['PriceGrowthRate'] = df["CloseNorm_compare"].pct_change() * 100
+# df['PriceGrowthRate_pct'] = df["CloseNorm_compare_pct"].pct_change() * 100
 df['TRGrowthRate'] = df["TRNorm_compare"].pct_change(fill_method=None) * 100
 
 #Terminal Data
@@ -143,6 +136,16 @@ print(df[[etfs[1]+'-PriceReturn',
 
 print('...')
 
+# print('Average compare, Price_pct: ' + str(df['CloseNorm_compare_pct'].mean()))
+# print('Percentage of Price beating the underlying: ' + str(percentOverOne_pct))
+# print('Price Ratio Change AVG: ' +  str(df['PriceGrowthRate_pct'].mean() * 100))
+# print(df[[etfs[1]+'-PriceReturn', 
+#             etfs[0]+'-PriceReturn', 
+#             etfs[1]+'-PR_pct_change', 
+#             etfs[0]+'-PR_pct_change']].iloc[-1])
+
+# print('...')
+
 print('Average compare, Total Return: ' + str(df['TRNorm_compare'].mean()))
 print('Percentage of TR beating the underlying: ' + str(TRpercentOverOne))
 print('Total Return Ratio Change AVG: ' +  str(df['TRGrowthRate'].mean() * 100))
@@ -157,35 +160,15 @@ print(df[[etfs[1]+'-TRActual',
 # print('Total Dividends & price gain test: ' + str(df[etfs[1] + '-Divs'].sum()) + ', ' + str(etf1gain))
 # print('Percentage difference in price gains: ' + str(percentDifferent))
 
-# firstPR = df[etfs[1] + '-AdjClose'].iloc[0] / df[etfs[0] + '-AdjClose'].iloc[0]
-# lastPR = df[etfs[1] + '-AdjClose'].iloc[-1] / df[etfs[0] + '-AdjClose'].iloc[-1]
-# print('1st price ratio: ' + str(firstPR))
-# print('last price ratio: ' + str(lastPR))
-
-# df[etfs[1] + '-AdjClose'].iloc[0] / df[etfs[0] + '-AdjClose'].iloc[0]
-####
-# print('Test Yield: ' + str(df[etfs[1] + '-Yield'].iloc[-1]))
-
-# print('compare to check: ' + str(countOverOne / len(df["AdjNorm_compare"]) * 100))
-# print(df.head(15))
-# print(df.tail(15))
-# print(df.to_string())
-
 ###FIGURE INFO
 plt.figure(figsize=(10,5))
 
 x_num = np.arange(len(df))
 # m1, b1 = np.polyfit(x_num, df['AdjNorm_compare'], 1)
 m1, b1 = np.polyfit(x_num, df['CloseNorm_compare'], 1)
-m2, b2 = np.polyfit(x_num, df['TRNorm_compare'], 1)
+m2, b2 = np.polyfit(x_num, df['TRNorm_compare'], 1) 
 
 #comparisons and trendlines
-#price trends
-
-
-# plt.plot(df['Date'], df['AdjNorm_compare'], label='Price', color='red', alpha=1)
-
-
 plt.plot(df['Date'], df['CloseNorm_compare'], label='Price', color='blue', alpha=1)
 plt.plot(df['Date'], m1 * x_num + b1, linestyle="dashed", label='PriceTrend', color='blue', alpha=0.9)
 
@@ -196,9 +179,6 @@ print('...')
 print('Price trendline: y= ' + str(m1) + ' * x + ' + str(b1))
 print('...')
 print('TR trendline: y= ' + str(m2) + ' * x + ' + str(b2))
-#TR trends
-# plt.plot(df['Date'], df['TRNorm_compare'], label='Total Return', color='red', alpha=1)
-# plt.plot(df['Date'], m2 * x_num + b2, linestyle="dashed", label='TotalReturnTrend', color='red', alpha=0.9)
 
 # #prices
 # plt.plot(df['Date'], df[etfs[1] + '-Close'] / 100, label='Test Price', color='green', alpha=0.9)
