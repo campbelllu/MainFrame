038968a6e73e77ec93a4d232b7bbcd27d10cba18
Luke <campbelllu3@gmail.com>
2025-09-07 00:08:41 -0700

    TR problems solved, finalizing this week to begin comparisons.

--- mainframe/investor_center/navCheck.py
+++ mainframe/investor_center/navCheck.py
@@ -27,17 +27,40 @@ import yfinance as yf
 
 ###################################################
 
+#used to test .pct_change function
+def grManualCalc(df_col):
+    try:
+        origList = df_col.tolist()
+        growthList = []
+        growthList.append(np.nan)
+        for x in range(len(origList)-1):
+            diff = abs((origList[x+1])-origList[x])
+            if origList[x] > origList[x+1]:
+                diff = diff * -1
+            if abs(origList[x]) == 0:
+                change = np.nan
+            else:
+                change = diff / abs(origList[x]) * 100
+            growthList.append(change)
+         
+        return growthList
+
+    except Exception as err:
+        print("grManualCalc error: ")
+        print(err)
+
+
 # Define ETFs to compare 
 etfs = ['SPY', "SPYI"] # # Example ETFs ; iwy==ymag ##First underlying, second the one to test against it
-time_period = "3y" #mo, y
+time_period = "1y" #mo, y
 start_date = "2023-01-01"
-end_date = "2024-12-31"
+end_date = "2025-12-31"
 
 # Fetch historical price data for each ETF 
 etf_data = {} 
 for etf in etfs: 
-    # data = yf.Ticker(etf).history(period=time_period) # Fetch data from time_period
-    data = yf.Ticker(etf).history(start=start_date, end=end_date) # Fetch data from start to end date
+    # data = yf.Ticker(etf).history(period=time_period, auto_adjust=False) # Fetch data from time_period
+    data = yf.Ticker(etf).history(start=start_date, end=end_date, auto_adjust=False) # Fetch data from start to end date
 
   
     etf_data[etf + '-Close'] = data["Close"] # Store only closing prices 
@@ -56,13 +79,13 @@ for etf in etfs:
         # ((end price - initial price) + distributions over that time) / initial price
         #((data["Close"].iloc[i] - data["Close"].iloc[0]) + data['Dividends'].iloc[0].cumsum()) / data["Close"].iloc[0]
         TRAlist.append(round(TRA,5))
-        TRA_pct_change = TRA / first_close_price * 100
+        TRA_pct_change = (TRA / first_close_price) * 100
         TRA_pct_change_list.append(round(TRA_pct_change,5))
 
         #Finding price change data
         priceReturn = etf_data[etf + '-Close'].iloc[i] - first_close_price
         price_return.append(round(priceReturn,5))
-        PR_pct_change = priceReturn / first_close_price * 100
+        PR_pct_change = (priceReturn / first_close_price) * 100
         price_return_pct_change.append(round(PR_pct_change,5))
 
     etf_data[etf + '-PriceReturn'] = price_return
@@ -77,6 +100,12 @@ etf_data[etfs[0]+ '-PR_pct_change'][0] = 1.00000
 etf_data[etfs[1] + '-PR_pct_change'][0] = 0.00001
 # df_etfD = pd.DataFrame(etf_data)
 # print(df_etfD.to_string())
+
+#LUKE
+#TR is wrong somehow. total return isn't calculated correctly because the tested's are beating the underlying by way too much. inaccurate twice.
+#also, add CAGR to TR calcs
+
+#cagr = (v_final/v_initial)^(1/t) - 1, t is  years
 
 # Combine into a single DataFrame with dates aligned, drop nulls to avoid lack of data due to inception dates
 df = pd.DataFrame(etf_data).dropna(how="any")
@@ -95,8 +124,27 @@ df['CloseNorm_compare'] = df['CloseComparison'] / df['CloseComparison'].iloc[0]
 # df['CloseComparison_pct'] = df[etfs[1] + '-PR_pct_change'] / df[etfs[0] + '-PR_pct_change']
 # df['CloseNorm_compare_pct'] = df['CloseComparison_pct'] / df['CloseComparison_pct'].iloc[1]
 #TR compare
-df['TRComparison'] = df[etfs[1] + '-TR_pct_change'] / df[etfs[0] + '-TR_pct_change']
-df['TRNorm_compare'] = (df['TRComparison'] / df['TRComparison'].iloc[1])
+
+#working on a fix. 
+df['NoAbs'] = (df[etfs[1] + '-TR_pct_change'] - df[etfs[0] + '-TR_pct_change'])
+df['NoAbs_gr'] = grManualCalc(df['NoAbs'])
+df['TRNorm_compare'] = (df[etfs[1] + '-TR_pct_change'] - df[etfs[0] + '-TR_pct_change']).abs()
+# df['TRNorm_compare'] = (df['TRComparison'] / df['TRComparison'].iloc[1])
+
+#LUKE
+#we need to get growth rate of no absolute value column. the no absolute column is the way to go honestly. gets you useful data from it, even if the graph is whatever. 
+#edit those print statements and the values therein. make it super useful. eventually we can log these into a DB and compare funds. win!
+
+# print(df[['Date', 'NoAbs', 'TRComparison', 'TRNorm_compare']].to_string())
+
+# data1 = {'Column1': (df[etfs[1] + '-TR_pct_change'] - df[etfs[0] + '-TR_pct_change']),
+#         'Column2': (df[etfs[1] + '-TR_pct_change'] - df[etfs[0] + '-TR_pct_change']).abs()}
+# guh = pd.DataFrame(data1)
+# print(guh.to_string)
+
+##Classic total return compare
+# df['TRComparison'] = df[etfs[1] + '-TR_pct_change'] / df[etfs[0] + '-TR_pct_change']
+# df['TRNorm_compare'] = (df['TRComparison'] / df['TRComparison'].iloc[1])
 
 # print(df[['TRComparison', 'TRNorm_compare']].to_string())
 # print(df['TRNorm_compare'].to_string())
@@ -107,9 +155,6 @@ df['TRNorm_compare'] = (df['TRComparison'] / df['TRComparison'].iloc[1])
 # print(df[['Date', etfs[1]+'-PriceReturn', etfs[1]+'-PR_pct_change', etfs[1]+'-TRActual', etfs[1]+'-TR_pct_change']].to_string())
 # print(df[['Date', etfs[1] + '-PR_pct_change', etfs[0] + '-PR_pct_change', 'CloseComparison_pct', 'CloseNorm_compare_pct']].to_string())
 
-#LUKE
-#TR is wrong somehow. total return isn't calculated correctly because the tested's are beating the underlying by way too much. inaccurate twice.
-
 #Get percentage of time that fund beat the underlying
 percentOverOne = (df["CloseNorm_compare"] > 1).mean() * 100
 # percentOverOne_pct = (df["CloseNorm_compare_pct"] > 1).mean() * 100
@@ -117,42 +162,20 @@ percentOverOne = (df["CloseNorm_compare"] > 1).mean() * 100
 TRpercentOverOne = (df["TRNorm_compare"] > 1).mean() * 100
 # TRpercentOverOneLastTen = (df["TRNorm_compare"].iloc[-10:] > 1).mean() * 100
 
+#Get Mean and Median from difference in total return percentages
+medianTRdiff = df['NoAbs'].median()
+averageTRdiff = df['NoAbs'].mean()
+
 #growth rates
 # df['PriceGrowthRate'] = df["AdjNorm_compare"].pct_change() * 100
-df['PriceGrowthRate'] = df["CloseNorm_compare"].pct_change() * 100
+# df['PriceGrowthRate'] = df["CloseNorm_compare"].pct_change() * 100
+df['PriceGrowthRate'] = grManualCalc(df["CloseNorm_compare"])#.pct_change() * 100
 # df['PriceGrowthRate_pct'] = df["CloseNorm_compare_pct"].pct_change() * 100
-df['TRGrowthRate'] = df["TRNorm_compare"].pct_change(fill_method=None) * 100
 
-#Terminal Data
-# print('Average compare: ' + str(df['AdjNorm_compare'].mean()))
-# print('Average compare, last 10: ' + str(df['AdjNorm_compare'].iloc[-10:].mean()))
-print('Average compare, Price: ' + str(df['CloseNorm_compare'].mean()))
-print('Percentage of Price beating the underlying: ' + str(percentOverOne))
-print('Price Ratio Change AVG: ' +  str(df['PriceGrowthRate'].mean() * 100))
-print(df[[etfs[1]+'-PriceReturn', 
-            etfs[0]+'-PriceReturn', 
-            etfs[1]+'-PR_pct_change', 
-            etfs[0]+'-PR_pct_change']].iloc[-1])
+# df['TRGrowthRate'] = df["TRNorm_compare"].pct_change(fill_method=None) * 100
+df['TRGrowthRate'] = grManualCalc(df["TRNorm_compare"])#.pct_change(fill_method=None) * 100
 
-print('...')
-
-# print('Average compare, Price_pct: ' + str(df['CloseNorm_compare_pct'].mean()))
-# print('Percentage of Price beating the underlying: ' + str(percentOverOne_pct))
-# print('Price Ratio Change AVG: ' +  str(df['PriceGrowthRate_pct'].mean() * 100))
-# print(df[[etfs[1]+'-PriceReturn', 
-#             etfs[0]+'-PriceReturn', 
-#             etfs[1]+'-PR_pct_change', 
-#             etfs[0]+'-PR_pct_change']].iloc[-1])
-
-# print('...')
-
-print('Average compare, Total Return: ' + str(df['TRNorm_compare'].mean()))
-print('Percentage of TR beating the underlying: ' + str(TRpercentOverOne))
-print('Total Return Ratio Change AVG: ' +  str(df['TRGrowthRate'].mean() * 100))
-print(df[[etfs[1]+'-TRActual', 
-            etfs[0]+'-TRActual',
-            etfs[1]+'-TR_pct_change', 
-            etfs[0]+'-TR_pct_change']].iloc[-1])
+#LUKE do you want to do a dividend comparison tool next?
 # print('Percentage TR beating the underlying: ' + str(TRpercentOverOne))
 # print('Percentage TR beating the underlying, last 10: ' + str(TRpercentOverOneLastTen))
 # print('TR Ratio Change AVG: ' +  str(df['TRGrowthRate'].mean() * 100))
@@ -160,41 +183,126 @@ print(df[[etfs[1]+'-TRActual',
 # print('Total Dividends & price gain test: ' + str(df[etfs[1] + '-Divs'].sum()) + ', ' + str(etf1gain))
 # print('Percentage difference in price gains: ' + str(percentDifferent))
 
+# print(df[['Date', etfs[1]+'-Close']].to_string())
 ###FIGURE INFO
-plt.figure(figsize=(10,5))
 
-x_num = np.arange(len(df))
-# m1, b1 = np.polyfit(x_num, df['AdjNorm_compare'], 1)
-m1, b1 = np.polyfit(x_num, df['CloseNorm_compare'], 1)
-m2, b2 = np.polyfit(x_num, df['TRNorm_compare'], 1) 
+def priceGraph():
+    # plt.figure(figsize=(10,5))
+    plt.subplot(1,2,1)
 
-#comparisons and trendlines
-plt.plot(df['Date'], df['CloseNorm_compare'], label='Price', color='blue', alpha=1)
-plt.plot(df['Date'], m1 * x_num + b1, linestyle="dashed", label='PriceTrend', color='blue', alpha=0.9)
+    x_num = np.arange(len(df))
+    # m1, b1 = np.polyfit(x_num, df['AdjNorm_compare'], 1)
+    m1, b1 = np.polyfit(x_num, df['CloseNorm_compare'], 1)
+    # m2, b2 = np.polyfit(x_num, df['TRNorm_compare'], 1) 
 
-plt.plot(df['Date'], df['TRNorm_compare'], label='TR', color='red', alpha=1)
-plt.plot(df['Date'], m2 * x_num + b2, linestyle="dashed", label='TR_Trend', color='red', alpha=0.9)
+    #comparisons and trendlines
+    plt.plot(df['Date'], df['CloseNorm_compare'], label='Price', color='blue', alpha=1)
+    plt.plot(df['Date'], m1 * x_num + b1, linestyle="dashed", label='PriceTrend', color='blue', alpha=0.9)
 
-print('...')
-print('Price trendline: y= ' + str(m1) + ' * x + ' + str(b1))
-print('...')
-print('TR trendline: y= ' + str(m2) + ' * x + ' + str(b2))
+    # plt.plot(df['Date'], df['TRNorm_compare'], label='TR', color='red', alpha=1)
+    # plt.plot(df['Date'], m2 * x_num + b2, linestyle="dashed", label='TR_Trend', color='red', alpha=0.9)
+    print('Average compare, Price: ' + str(df['CloseNorm_compare'].mean()))
+    print('Percentage of Price beating the underlying: ' + str(percentOverOne) + '%')
+    print('Price Ratio Change AVG: ' +  str(df['PriceGrowthRate'].mean() * 100))
+    # print('My Price Ratio Change AVG: ' +  str(df['MYPriceGrowthRate'].mean() * 100))
+    print(df[[etfs[1]+'-Close', 
+                etfs[0]+'-Close']].iloc[0] )
+    print(df[[etfs[1]+'-Close', 
+                etfs[0]+'-Close', 
+    etfs[1]+'-PriceReturn', 
+            etfs[0]+'-PriceReturn', 
+            etfs[1]+'-PR_pct_change', 
+            etfs[0]+'-PR_pct_change']].iloc[-1])
 
-# #prices
-# plt.plot(df['Date'], df[etfs[1] + '-Close'] / 100, label='Test Price', color='green', alpha=0.9)
-# plt.plot(df['Date'], df[etfs[0] + '-Close'] / 3000, label='Target Price', color='orange', alpha=0.9)
+    print('...')
+    print('Price trendline: y= ' + str(m1) + ' * x + ' + str(b1))
+    print('...')
+    # print('TR trendline: y= ' + str(m2) + ' * x + ' + str(b2))
 
-#Graph info
-plt.title('Relationship of ' + etfs[1] + '/' + etfs[0])
-plt.xlabel('Date')
-plt.ylabel('Normalized Relation Ratios')
-plt.legend()
-# plt.ioff()
-plt.grid(True)
+    # print(df[[  etfs[1]+'-Close', 
+    #             etfs[0]+'-Close',
+    #             etfs[1]+'-TRActual', 
+    #             etfs[0]+'-TRActual'
+    #             ]].iloc[2]) 
+
+
+    # #prices
+    # plt.plot(df['Date'], df[etfs[1] + '-Close'] / 100, label='Test Price', color='green', alpha=0.9)
+    # plt.plot(df['Date'], df[etfs[0] + '-Close'] / 3000, label='Target Price', color='orange', alpha=0.9)
+
+    #Graph info
+    plt.title('Price Relationship of ' + etfs[1] + '/' + etfs[0])
+    plt.xlabel('Date')
+    plt.ylabel('Normalized Relation Ratios')
+    plt.legend()
+    # plt.ioff()
+    plt.grid(True)
+    # plt.show()
+
+def TRGraph():
+    # plt.figure(figsize=(10,5))
+    plt.subplot(1,2,2)
+
+    x_num = np.arange(len(df))
+    # m1, b1 = np.polyfit(x_num, df['AdjNorm_compare'], 1)
+    # m1, b1 = np.polyfit(x_num, df['CloseNorm_compare'], 1)
+    m2, b2 = np.polyfit(x_num, df['TRNorm_compare'], 1) 
+
+    #comparisons and trendlines
+    # plt.plot(df['Date'], df['CloseNorm_compare'], label='Price', color='blue', alpha=1)
+    # plt.plot(df['Date'], m1 * x_num + b1, linestyle="dashed", label='PriceTrend', color='blue', alpha=0.9)
+
+    plt.plot(df['Date'], df['TRNorm_compare'], label='TR', color='red', alpha=1)
+    plt.plot(df['Date'], m2 * x_num + b2, linestyle="dashed", label='TR_Trend', color='red', alpha=0.9)
+
+    # print('...')
+    # print('Price trendline: y= ' + str(m1) + ' * x + ' + str(b1))
+
+    print('Average compare, Total Return: ' + str(df['TRNorm_compare'].mean()))
+    print('Percentage of TR beating the underlying: ' + str(TRpercentOverOne))
+    print('Total Return Ratio Change AVG: ' +  str(df['TRGrowthRate'].mean() * 100))
+    print('Mean difference in TR: ' + str(averageTRdiff))
+    print('Median difference in TR: ' + str(medianTRdiff))
+    print(df[[  etfs[1]+'-TRActual', 
+                etfs[0]+'-TRActual',
+                etfs[1]+'-TR_pct_change', 
+                etfs[0]+'-TR_pct_change'
+                ]].iloc[-1])   #'TRNorm_compare'  'Date', etfs[1]+'-Close', etfs[1] + '-TRScuffed', etfs[0]+'-Close', etfs[0] + '-TRScuffed',
+    print('...')
+    print('TR trendline: y= ' + str(m2) + ' * x + ' + str(b2))
+    print('...')
+
+    # print(df[[  etfs[1]+'-Close', 
+    #             etfs[0]+'-Close',
+    #             etfs[1]+'-TRActual', 
+    #             etfs[0]+'-TRActual'
+    #             ]].iloc[2]) 
+
+
+    # #prices
+    # plt.plot(df['Date'], df[etfs[1] + '-Close'] / 100, label='Test Price', color='green', alpha=0.9)
+    # plt.plot(df['Date'], df[etfs[0] + '-Close'] / 3000, label='Target Price', color='orange', alpha=0.9)
+
+    #Graph info
+    plt.title('TR Relationship of ' + etfs[1] + '/' + etfs[0])
+    plt.xlabel('Date')
+    plt.ylabel('Normalized Relation Ratios')
+    plt.legend()
+    # plt.ioff()
+    plt.grid(True)
+    # plt.show()
+
+
+plt.figure(figsize=(15,7))
+
+priceGraph()
+TRGraph()
+
+plt.tight_layout()
 plt.show()
+
+
 # plt.savefig('justagraph3.png')
-
-
 # Display DataFrame 
 # print(etfs)
 # print(etfs[1])
